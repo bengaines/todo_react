308073b04b06b199c61695bc91f13e0404101387
Ben Gaines <bengaines92@gmail.com>
2017-04-10 14:48:47 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id:1, name: 'Learn JSX', isComplete: true},
+        {id:2, name: 'Build awesome apps', isComplete: false},
+        {id:3, name: 'Ship it!', isComplete: false}
+      ],
+      currentTodo: ''
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+            <input type="text" value={this.state.currentTodo}/>
+            <div className="Todo-List">
+              <ul>
+                {this.state.todos.map(todo =>
+                  <li key={todo.id}>
+                    <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+                  </li>)}
+              </ul>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
